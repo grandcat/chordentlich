8d769fa42d7eb5f6c54a3d74c84488205dce6fe2
Stefan S <smarzlys@gmail.com>
2015-07-03 11:43:14 +0200

    Further tests for finger table

--- code/fingerTable.py
+++ code/fingerTable.py
@@ -4,25 +4,34 @@ import hashlib
 import math
 import datetime
 
-chordFingerTableSize = 256;
+chordFingerTableSize = 4;
 chordRingSize = 2**chordFingerTableSize; # Maximum number of addresses in the Chord network
 
 class Finger():
-	
+	def __init__(self, nodeId=None):
+		# Start: keys from [ finger[this], finger[this+1] ]
+		self.start = None
+		# Successor
+		self.successor_nodeId = nodeId
+		self.successor_host = None
 
 class FingerTable():
-
 	def __init__(self, nodeId):
-		self.nodeId = int(nodeId, 16) # make integer for calculations
+		self.nodeId = int(nodeId, 16) % chordRingSize # make integer for calculations
 		self.fingerTable = []
+
 	def getEntry(self, position):
 		addition = 2**(position-1)
 		entry = (self.nodeId + addition) % chordRingSize
 		return hex(entry)
 
-	def initFingerTable(self):
+	def initFingerTable(self, node_successor):
+		# for k in range(0, chordFingerTableSize):
+		self.addEntry(Finger(nodeId=node_successor))
 
-		self.fingerTable.append()
+	def addEntry(self, finger_obj):
+		self.fingerTable.append(finger_obj)
+
 
 	def getClosestPrecedingFinder(self, searchKey):
 		for k in range(chordFingerTableSize, 0, -1):
@@ -32,7 +41,7 @@ class FingerTable():
 		return hex(self.nodeId)
 
 	def getNodeId(self):
-	    return self.nodeId
+		return self.nodeId
 
 nodeIdToSearch = hashlib.sha256("213123213213".encode()).hexdigest();
 
@@ -47,3 +56,5 @@ print (ft1.getEntry(3))
 print (ft1.getEntry(4))
 print (ft1.getEntry(5))
 print (ft1.getEntry(6))
+
+
