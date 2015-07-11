77cce6e2f09fe88d988227f00777e56bca8a1592
Manuel Schultheiss <manukru@gmail.com>
2015-07-11 15:41:38 +0200

    changes tabs to spaces

--- code/dhtServer.py
+++ code/dhtServer.py
@@ -8,10 +8,12 @@ import helpers
 import asyncio
 import hashlib
 from multiprocessing import Process
-import getopt, sys
+import getopt
+import sys
 import json
 from jsonschema import validate, Draft4Validator
 import msg_schemata
+import fingerTable
 
 schema = {
     "action": {"type": "string"},
@@ -21,110 +23,121 @@ schema = {
 PORT_START = None
 SERVER_COUNT = None
 
+
 class DHTAsyncClient(asyncio.Protocol):
-	def __init__(self, msg, loop4):
-		self.msg = msg
-		self.loop4 = loop4
 
-	def connection_made(self, transport):
-		print('  Connected ', transport.get_extra_info('peername'))
-		self.transport = transport
-		# transport.write(self.msg.encode())
+    def __init__(self, msg, loop4):
+        self.msg = msg
+        self.loop4 = loop4
 
-	def data_received(self, data):
-		# Send message to server
-		msg = data.decode()
-		print('Server returned: ', msg)
-		self.transport.write(data)
-		self.transport.close()
+    def connection_made(self, transport):
+        print('  Connected ', transport.get_extra_info('peername'))
+        self.transport = transport
+        # transport.write(self.msg.encode())
 
-	def connection_lost(self, exc):
-		# self.loop4.stop()
-		print("Client  Connection Lost")
+    def data_received(self, data):
+        # Send message to server
+        msg = data.decode()
+        print('Server returned: ', msg)
+        self.transport.write(data)
+        self.transport.close()
+
+    def connection_lost(self, exc):
+        # self.loop4.stop()
+        print("Client  Connection Lost")
 
 
 class DHTAsyncServer(asyncio.Protocol):
 
-	def __init__(self, server_address, port):
-		self.server_address = server_address
-		self.port = port
+    def __init__(self, server_address, port, bootstrapNode=None):
+        self.server_address = server_address
+        self.port = port
+        self.fingerTable = fingerTable.FingerTable(self.get_key())
+        self.__serverConnections = {}  # remember active connections
+        self.bootstrapNode = bootstrapNode
+        print("Bootstrap Node")
+        print("My key: ", self.get_key())
 
-		self.__serverConnections = {}  # remember active connections
+    @asyncio.coroutine
+    def send_data(self, data):
 
-		print("My key: ", self.get_key())
+        print("  send data back..." + data)
+        server2 = self.__serverConnections.get("1338")
 
-	@asyncio.coroutine
-	def send_data(self, data):
+        if server2 is None or not server2.connected:
+            protocol, server2 = yield from loop.create_connection(lambda: DHTAsyncClient(data, loop), '127.0.0.1', 1338)
+            server2.server_transport = self.transport
+            self.__serverConnections["1338"] = server2
+            print("Server2: ", data)
 
-		print("  send data back..." + data)
-		server2 = self.__serverConnections.get("1338")
+        server2.transport.write(data.encode())
+        # server2.transport.close()
 
-		if server2 is None or not server2.connected:
-			protocol, server2 = yield from loop.create_connection(lambda: DHTAsyncClient(data, loop), '127.0.0.1', 1338)
-			server2.server_transport = self.transport
-			self.__serverConnections["1338"] = server2
-			print("Server2: ", data)
+    def connection_made(self, transport):
+
+        print("new connection")
+        peer = transport.get_extra_info('peername')
+        self.transport = transport
+
+    def data_received(self, data):
+        # Warning: do not call data.decode() twice
+        # print("DATA IS" + data.decode())
+        print("Start data_received.")
+        try:
+            print("Raw data: " + str(data))
+            msg = json.loads(data.decode())
+            print(msg)
+        except Exception as e:
+            print("JSON problem: " + str(e))
+        if msg["action"] == "client_start":
+            print("GOT DHT STORE COMMAND")
+
+            # Firs twe JOIN the Chord network. Therefore we initialize the
+            # finger Table with start values
+
+            # is it a botstrap node?
+
+            for i in range(fingerTable.chordFingerTableSize):
+                self.fingerTable.entries.append("123")
 
 
-		server2.transport.write(data.encode())
-		# server2.transport.close()
+            message = json.dumps({
+                "action": "FIND_SUCCESSOR",
+                "key": self.get_keytemp("127.0.0.1", 1338),
+                #"source_identity": self.get_key(),
+                "source_port": self.port,
+                "source_ip" : self.server_address
+            })
+            asyncio.Task(self.send_data(message))
 
-	def connection_made(self, transport):
+        elif msg["action"] == "FIND_SUCCESSOR":
+            print("got find successor request")
+            if msg["key"] == self.get_key():
+                # We are the target
+                print("Sending reply to origin")
+            else:
+                # Forward message to next peer
+                new_msg = copy.deepcopy(msg)
+                # TODO: add trace
+                asyncio.Task(self.send_data(new_msg))
 
-		print("new connection")
-		peer = transport.get_extra_info('peername')
-		self.transport = transport
+        self.transport.write(json.dumps({
+            "STATUS": "OK"
+        }).encode())  # send back
+        self.transport.close()
 
-	def data_received(self, data):
-		# Warning: do not call data.decode() twice
-		# print("DATA IS" + data.decode())
-		print("Start data_received.")
-		try:
-			print("Raw data: " + str(data))
-			msg = json.loads(data.decode())
-			print(msg)
-		except Exception as e:
-			print("JSON problem: " + str(e))
-		if msg["action"] == "client_debug":
-			print("GOT DHT STORE COMMAND")
+    def get_next_server(self):
+        ip_address = "localhost"
+        port = self.port + 1
 
-			message = json.dumps({
-				"action": "FIND_SUCCESSOR",
-				"key": self.get_keytemp("127.0.0.1", 1338),
-				#"source_identity": self.get_key(),
-				"source_port": self.port,
-				"source_ip" : self.server_address
-			})
-			asyncio.Task(self.send_data(message))
+        return ip_address, port
 
-		elif msg["action"] == "FIND_SUCCESSOR":
-			print("got find successor request")
-			if msg["key"] == self.get_key():
-				# We are the target
-				print("Sending reply to origin")
-			else:
-				# Forward message to next peer
-				new_msg = copy.deepcopy(msg)
-				# TODO: add trace
-				asyncio.Task(self.send_data(new_msg))
+    # TODO: public key hash
+    def get_key(self):
+        return hashlib.sha256((self.server_address + str(self.port)).encode()).hexdigest()
 
-		self.transport.write(json.dumps({
-			"STATUS": "OK"
-		}).encode())  # send back
-		self.transport.close()
-
-	def get_next_server(self):
-		ip_address = "localhost"
-		port = self.port + 1
-
-		return ip_address, port
-
-	# TODO: public key hash
-	def get_key(self):
-		return hashlib.sha256((self.server_address + str(self.port)).encode()).hexdigest()
-
-	def get_keytemp(self, address, port):
-		return hashlib.sha256((address + str(port)).encode()).hexdigest()
+    def get_keytemp(self, address, port):
+        return hashlib.sha256((address + str(port)).encode()).hexdigest()
 
 
 
@@ -132,12 +145,12 @@ class DHTAsyncServer(asyncio.Protocol):
 opts, args = getopt.getopt(sys.argv[1:], "p:s:c:")
 port = None
 for key, val in opts:
-	if key == "-p":
-		port = int(val)
-	elif key == "-s":
-		PORT_START = int(val)
-	elif key == "-c":
-		SERVER_COUNT = int(val)
+    if key == "-p":
+        port = int(val)
+    elif key == "-s":
+        PORT_START = int(val)
+    elif key == "-c":
+        SERVER_COUNT = int(val)
 
 print("Port", port)
 print("-------------------")
@@ -146,41 +159,43 @@ loop.set_debug(True)
 
 @asyncio.coroutine
 def initialize(loop):
-	# TODO: improve passing of parameters
-	dhtServer = yield from loop.create_server(lambda: DHTAsyncServer('127.0.0.1', port), '127.0.0.1', port)
-	if port == 1339:
-		# make a local client
-		threading.Thread(target=connectClient).start()
+    # TODO: improve passing of parameters
 
+    boostrapNodePort = 1339 if port==1339 else None
+    dhtServer = yield from loop.create_server(lambda: DHTAsyncServer('127.0.0.1', port, bootstrapNode=boostrapNodePort), '127.0.0.1', port)
+    if port == 1339:
+        # make a local client
+        threading.Thread(target=connectClient).start()
 
 def connectClient():
-	print("1")
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_address = ('127.0.0.1', 1339)
-	sock.connect(server_address)
-	print("2")
-	try:
+    print("1")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('127.0.0.1', 1339)
+    sock.connect(server_address)
+    print("2")
+    try:
 
-		message = json.dumps({
-			"action": "client_debug",
-			# "action": "FIND_SUCCESSOR",
-		})
-		# validate(message, schema)
-		sock.sendall(bytes(message, 'UTF-8'))
+        message = json.dumps({
+            "action": "client_start",
 
-		amount_received = 0
-		amount_expected = len(message)
+            # "action": "FIND_SUCCESSOR",
+        })
 
-		while amount_received < amount_expected:
-			data = sock.recv(16)
-			amount_received += len(data)
+        # validate(message, schema)
+        sock.sendall(bytes(message, 'UTF-8'))
 
-		print("client server got" + str(data))
+        amount_received = 0
+        amount_expected = len(message)
 
-	finally:
-		sock.close()
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+
+        print("client server got" + str(data))
+
+    finally:
+        sock.close()
 
 # Main
 asyncio.Task(initialize(loop))
 loop.run_forever()
-
--- code/fingerTable.py
+++ code/fingerTable.py
@@ -4,46 +4,40 @@ import hashlib
 import math
 import datetime
 
-chordFingerTableSize = 256;
+chordFingerTableSize = 6; # TODO: 256
 chordRingSize = 2**chordFingerTableSize; # Maximum number of addresses in the Chord network
 
-class Finger():
-	
-
 class FingerTable():
+    def __init__(self, nodeId):
+        self.nodeId = int(nodeId, 16) # make integer for calculations
+        self.entries = []
+    def getEntry(self, position):
+        addition = 2**(position-1)
+        entry = (self.nodeId + addition) % chordRingSize
+        return hex(entry)
+    def initFingerTable(self):
+        pass
+        #self.fingerTable.append()
+    def getClosestPrecedingFinder(self, searchKey):
+        for k in range(chordFingerTableSize, 0, -1):
+            if  int(searchKey, 16) > int(self.getEntry(k), 16):
+                return self.getEntry(k+1)
 
-	def __init__(self, nodeId):
-		self.nodeId = int(nodeId, 16) # make integer for calculations
-		self.fingerTable = []
-	def getEntry(self, position):
-		addition = 2**(position-1)
-		entry = (self.nodeId + addition) % chordRingSize
-		return hex(entry)
+        return hex(self.nodeId)
 
-	def initFingerTable(self):
+    def getNodeId(self):
+        return self.nodeId
 
-		self.fingerTable.append()
-
-	def getClosestPrecedingFinder(self, searchKey):
-		for k in range(chordFingerTableSize, 0, -1):
-			if  int(searchKey, 16) > int(self.getEntry(k), 16):
-				return self.getEntry(k+1)
-
-		return hex(self.nodeId)
-
-	def getNodeId(self):
-	    return self.nodeId
-
-nodeIdToSearch = hashlib.sha256("213123213213".encode()).hexdigest();
-
-nodeId = hashlib.sha256("213123213213".encode()).hexdigest();
-ft1 = FingerTable(nodeId)
-
-print (ft1.getClosestPrecedingFinder(nodeId))
-print (ft1.getClosestPrecedingFinder(nodeId))
-
-
-print (ft1.getEntry(3))
-print (ft1.getEntry(4))
-print (ft1.getEntry(5))
-print (ft1.getEntry(6))
+# nodeIdToSearch = hashlib.sha256("213123213213".encode()).hexdigest();
+#
+# nodeId = hashlib.sha256("213123213213".encode()).hexdigest();
+# ft1 = FingerTable(nodeId)
+#
+# print (ft1.getClosestPrecedingFinder(nodeId))
+# print (ft1.getClosestPrecedingFinder(nodeId))
+#
+#
+# print (ft1.getEntry(3))
+# print (ft1.getEntry(4))
+# print (ft1.getEntry(5))
+# print (ft1.getEntry(6))
