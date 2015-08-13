2debb24acc572b49cfcefb799d2d904adfe62e4f
Manuel Schultheiss <manukru@gmail.com>
2015-08-13 18:06:19 +0200

    testfix

--- code/test_Node.py
+++ code/test_Node.py
@@ -14,7 +14,7 @@ class TestNode(unittest.TestCase):
       self.assertTrue(in_interval(12, 0, 99999999999999999999999999999 ))
       self.assertTrue(in_interval(12, 222, 0 ))
       self.assertTrue(in_interval(12, 1, 1 ))
-      self.assertTrue(in_interval(12, 1111111111111111111111111111, 11111111111111111111111111111111 ))
+      self.assertFalse(in_interval(12, 1111111111111111111111111111, 11111111111111111111111111111111 ))
       self.assertFalse(in_interval(12, 123, 123123 ))
       self.assertFalse(in_interval(12, 321321312, 123123 ))
       self.assertFalse(in_interval(12, 13, 19 ))
