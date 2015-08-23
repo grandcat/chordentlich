0642c4ae94367b705c4f1deea40e6227b10b2fef
Manuel Schultheiss <manukru@gmail.com>
2015-08-23 21:14:23 +0200

    disable test interface, fixed gnome sh scripts

--- code/gnome_start.sh
+++ code/gnome_start.sh
@@ -5,14 +5,13 @@ sleep 0.5
 count=3
 startport=1338
 
-gnome-terminal -x python3 main.py -i 1337  -h "hostkeys/prv1.pem"
-
+gnome-terminal -x python3 main.py -i 1337
 
 echo "Start some nodes...."
 for (( p=0 ; p<$count; p++ )) # open 3 different chord nodes
 do
 	((port=$startport + $p))
 	echo "Starting node on $port with startport $startport"
-	gnome-terminal -x python3 main.py -i $port -b 1337 -B 127.0.0.1 -h "hostkeys/prv${port}.pem"
+	gnome-terminal -x python3 main.py -i $port -b 1337 -B 127.0.0.1
 	sleep 0.3
 done
